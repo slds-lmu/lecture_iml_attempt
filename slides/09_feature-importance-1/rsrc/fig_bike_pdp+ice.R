94099a85f0a43cb867ce5e11e086edf4880c305b
root <root@DESKTOP-AERTSMD.localdomain>
2025-06-20 16:26:55 +0200

    temporary fixes

--- slides/09_feature-importance-1/rsrc/fig_bike_pdp+ice.R
+++
@@ -1,8 +0,0 @@
-# PREREQ -----------------------------------------------------------------------
-source("bike_data.R")
-library("ggplot2")
-
-# PLOT -------------------------------------------------------------------------
-pdp <- FeatureEffects$new(predictor, method='pdp+ice')
-p  = pdp$plot(ncols=3) + theme_bw()
-ggsave('../figure_man/bike_pdp+ice.pdf', width=12, height=8)
